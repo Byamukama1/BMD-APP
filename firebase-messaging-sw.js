f7ac2dfd217323f0544d7bccc4452d82a78bc422
Byamukama1 <byamukamambabazimentor2@gmail.com>
2026-07-26 14:58:09 +0300

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,4 @@
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+
+// Firebase configuration will be added in the next step.
