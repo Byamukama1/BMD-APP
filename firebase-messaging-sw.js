6a31b9769cc91b7c95c6cb83b573d5860543d41a
Byamukama1 <byamukamambabazimentor2@gmail.com>
2026-07-26 15:00:41 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,14 @@
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
-// Firebase configuration will be added in the next step.
+firebase.initializeApp({
+    apiKey: "AIzaSyC-PINF2AJcETYIMINiCfT0gSxLxw70vSE",
+    authDomain: "bmd-app-1aec8.firebaseapp.com",
+    projectId: "bmd-app-1aec8",
+    storageBucket: "bmd-app-1aec8.firebasestorage.app",
+    messagingSenderId: "722780031230",
+    appId: "1:722780031230:web:cd19a98e24e4d9d80dfdaf",
+    measurementId: "G-WPEG8SQSW7"
+});
+
+const messaging = firebase.messaging();
